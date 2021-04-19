a4facda7d32d21a2a3a9c1e423bc579737cc9a1e
Miffy <61541547+wu.xinyi1@northeastern.edu>
2021-04-19 14:43:27 -0700

    version 1

---
+++ client.py
@@ -0,0 +1,17 @@
+import time
+from coapthon.client.helperclient import HelperClient
+
+host = "192.168.0.100"
+port = 5683
+path = "basic"
+
+def main():
+    client = HelperClient(server=(host, port))
+    while True :
+        response = client.put(path,"Hello world!")
+        print(response.pretty_print())
+        time.sleep(5) #5 seconds
+    client.stop()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import coapthon.server.coap
+from coapthon.resources.resource import Resource
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+Host = "0.0.0.0"
+Port = 5683
 
+class BasicResource(Resource):
+    def __init__(self, name="BasicResource", coap_server=None):
+        super(BasicResource, self).__init__(name, coap_server, visible=True,
+                                            observable=True, allow_children=True)
+        self.payload = "Basic Resource"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    def render_GET(self, request):
+        return self
 
+    def render_PUT(self, request):
+        self.payload = request.payload
+        return self
 
-# Press the green button in the gutter to run the script.
+    def render_POST(self, request):
+        res = BasicResource()
+        res.location_query = request.uri_query
+        res.payload = request.payload
+        return res
+
+    def render_DELETE(self, request):
+        return True
+
+class CoAPServer(coapthon.server.coap.CoAP):
+    def __init__(self, host, port):
+        coapthon.server.coap.CoAP.__init__(self, (host, port))
+        self.add_resource('basic', BasicResource())
+
+def main():
+    print("CoAPServer IP addr : %s port : %d "%(Host,Port))
+    server = CoAPServer(Host,Port)
+    try:
+        server.listen(10)
+    except KeyboardInterrupt:
+        print("Server Shutdown")
+        server.close()
+        print("Exiting...")
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
