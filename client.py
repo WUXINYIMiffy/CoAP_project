3e880e47b12a3134c38263e8c0221c2b4dcce6e6
Miffy <61541547+wu.xinyi1@northeastern.edu>
2021-04-29 15:52:55 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import time
 from coapthon.client.helperclient import HelperClient
 
-host = "192.168.0.129"
+host = "192.168.0.134"
 port = 5683
 path = "basic"
 
@@ -9,12 +9,17 @@ path = "basic"
 def main():
     client = HelperClient(server=(host, port))
 
-    for _ in range(50):
-        # response = client.put(path, "hello miffy.", _type=1)  # NON
-        response = client.put(path, "hello miffy.")  # CON
-        print(response.pretty_print())
-        # time.sleep(5)  # 5 seconds
-    client.stop()
+    try:
+        for _ in range(50):
+            #response = client.put(path, "hello miffy.", _type=1)  # NON
+            response = client.put(path, "hello miffy.")  # CON
+            if response is not None:
+                print(response.pretty_print())
+            else:
+                print('EMPTY RESPONSE')
+            # time.sleep(5)  # 5 seconds
+    finally:
+        client.stop()
 
 
 if __name__ == '__main__':
