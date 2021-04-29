d07800f1174edd7ee8efda8967e2d12edee5e483
Miffy <61541547+wu.xinyi1@northeastern.edu>
2021-04-29 10:51:44 -0700

    update

--- client.py
+++ client.py
@@ -1,17 +1,21 @@
 import time
 from coapthon.client.helperclient import HelperClient
 
-host = "192.168.0.100"
+host = "192.168.0.129"
 port = 5683
 path = "basic"
 
+
 def main():
     client = HelperClient(server=(host, port))
-    while True :
-        response = client.put(path,"hello miffy.")
-        print(response.pretty_print())
-        time.sleep(5) #5 seconds
-    client.stop()
+    try:
+        while True:
+            response = client.put(path, "hello miffy.", _type=1)
+            print(response.pretty_print())
+            time.sleep(5)  # 5 seconds
+    finally:
+        client.stop()
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import coapthon.server.coap
 from coapthon.resources.resource import Resource
+from coapthon.messages.request import Request
 
 Host = "0.0.0.0"
 Port = 5683
+
 
 class BasicResource(Resource):
     def __init__(self, name="BasicResource", coap_server=None):
@@ -10,36 +12,39 @@ class BasicResource(Resource):
                                             observable=True, allow_children=True)
         self.payload = "Basic Resource"
 
-    def render_GET(self, request):
+    def render_GET(self, request: Request):
         return self
 
-    def render_PUT(self, request):
+    def render_PUT(self, request: Request):
         self.payload = request.payload
         return self
 
-    def render_POST(self, request):
+    def render_POST(self, request: Request):
         res = BasicResource()
         res.location_query = request.uri_query
         res.payload = request.payload
         return res
 
-    def render_DELETE(self, request):
+    def render_DELETE(self, request: Request):
         return True
+
 
 class CoAPServer(coapthon.server.coap.CoAP):
     def __init__(self, host, port):
         coapthon.server.coap.CoAP.__init__(self, (host, port))
         self.add_resource('basic', BasicResource())
 
+
 def main():
-    print("CoAPServer IP addr : %s port : %d "%(Host,Port))
-    server = CoAPServer(Host,Port)
+    print("CoAPServer IP addr : %s port : %d " % (Host, Port))
+    server = CoAPServer(Host, Port)
     try:
         server.listen(10)
     except KeyboardInterrupt:
         print("Server Shutdown")
         server.close()
         print("bye")
+
 
 if __name__ == '__main__':
     main()
